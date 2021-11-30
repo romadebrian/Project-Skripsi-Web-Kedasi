b112690fe41f40d991ec997135419cb44157f644
romadebrian <roma_coll04@yahoo.com>
2021-11-30 14:29:16 +0700

    manager file and folder

--- src/pages/Index/App.js
+++ src/pages/Index/App.js
@@ -4,14 +4,14 @@ import Header from "./component/Header";
 import SideNav from "./component/SideNav";
 import Footer from "./component/Footer";
 
-import Home from "./component/Home";
-import Ruangan from "./component/pages/Ruangan";
-import Notifikasi from "./component/pages/Notifikasi";
-import Pesan from "./component/pages/Pesan";
-import Profile from "./component/pages/Profile";
+import Home from "../Home/Home";
+import Ruangan from "../Ruangan/Ruangan";
+import Notifikasi from "../Notifikasi/Notifikasi";
+import Pesan from "../Pesan/Pesan";
+import Profile from "../Profile/Profile";
 
-import DetailOrder from "./component/pages/props/Ruangan/DetailOrder";
-import PrintLaporan from "./component/pages/props/Ruangan/PrintLaporan";
+import DetailOrder from "../Ruangan/props/Ruangan/DetailOrder";
+import PrintLaporan from "../Ruangan/props/Ruangan/PrintLaporan";
 
 // Fungsi untuk yang di tampilkan ke index.html
 function App() {
