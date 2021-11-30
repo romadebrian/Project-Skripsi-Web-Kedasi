925b058ac7d1247f6f7c2bfb4c60da9f203becb7
romadebrian <roma_coll04@yahoo.com>
2021-11-30 14:55:54 +0700

    manage folder and file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import $ from "jquery";
-import Header from "./component/Header";
-import SideNav from "./component/SideNav";
-import Footer from "./component/Footer";
+import Header from "./component/Header/Header";
+import SideNav from "./component/SideNav/SideNav";
+import Footer from "./component/Footer/Footer";
 
-import Home from "../Home/Home";
-import Ruangan from "../Ruangan/Ruangan";
-import Notifikasi from "../Notifikasi/Notifikasi";
-import Pesan from "../Pesan/Pesan";
-import Profile from "../Profile/Profile";
+import Home from "./pages/Home/Home";
+import Ruangan from "./pages/Ruangan/Ruangan";
+import Notifikasi from "./pages/Notifikasi/Notifikasi";
+import Pesan from "./pages/Pesan/Pesan";
+import Profile from "./pages/Profile/Profile";
 
-import DetailOrder from "../Ruangan/props/Ruangan/DetailOrder";
-import PrintLaporan from "../Ruangan/props/Ruangan/PrintLaporan";
+import DetailOrder from "./pages/Ruangan/props/Ruangan/DetailOrder";
+import PrintLaporan from "./pages/Ruangan/props/Ruangan/PrintLaporan";
 
 // Fungsi untuk yang di tampilkan ke index.html
 function App() {
