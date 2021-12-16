af4b7e280f3b6870f6c8af5e8de2df7d60d5c316
romadebrian <roma_coll04@yahoo.com>
2021-12-16 17:41:31 +0700

    change to class compontnt

--- src/pages/Ruangan/PesanRuangan/PesanRuangan.js
+++ src/pages/Ruangan/PesanRuangan/PesanRuangan.js
@@ -1,175 +1,185 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { Component } from "react";
 
-function PesanRuangan(props) {
-  const [tanggalSekarang, setTanggalSekarang] = useState("");
-  const [tanggalSewa, setTanggalSewa] = useState("");
+class PesanRuangan extends Component {
+  state = {
+    tanggalSekarang: "",
+    tanggalSewa: "",
+  };
 
-  useEffect(() => {
+  componentDidMount() {
     window.$("#reservationdate").datetimepicker({
       format: "L",
     });
 
-    function ConvertTanggalSekarang() {
-      let hariini = new Date();
-      let tgl =
-        hariini.getDate() +
-        "-" +
-        parseInt(hariini.getMonth() + 1) +
-        "-" +
-        hariini.getFullYear();
-      setTanggalSekarang(tgl);
-      // console.log(new Date());
-    }
+    let hariini = new Date();
+    let tgl =
+      hariini.getDate() +
+      "-" +
+      parseInt(hariini.getMonth() + 1) +
+      "-" +
+      hariini.getFullYear();
 
-    ConvertTanggalSekarang();
-  });
+    this.setState({
+      tanggalSewa: tgl,
+    });
 
-  const formatDate = (e) => {
+    console.log(new Date());
+  }
+
+  formatDate = (e) => {
     let tanggalMulai = new Date(e.target[3].value);
     let TanggalSew = `${tanggalMulai.getDate()}-${
       tanggalMulai.getMonth() + 1
     }-${tanggalMulai.getFullYear()}`;
 
-    setTanggalSewa(TanggalSew);
+    this.setState({
+      tanggalSewa: TanggalSew,
+    });
 
-    // console.log(dateMDY);
+    console.log(this.state.tanggalSewa);
   };
 
-  const handleSubmit = (e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
-    formatDate(e);
 
-    setTimeout(function () {
-      console.log(e.target[0].value);
-      console.log(e.target[1].value);
-      console.log(e.target[2].value);
-      console.log(tanggalSewa);
-      // console.log(e.target[3].value);
-      // console.log(e.target[4].value);
-      console.log(e.target[5].checked);
-      console.log(e.target[6].checked);
-      console.log(e.target[7].checked);
-      console.log(e.target[8].checked);
-    }, 3000);
+    console.log(e.target[3].value);
+
+    this.formatDate(e);
+    // console.log(e.target[0].value);
+    // console.log(e.target[1].value);
+    // console.log(e.target[2].value);
+    // console.log(tanggalSewa);
+
+    // console.log(e.target[3].value);
+    // console.log(e.target[4].value);
+
+    // console.log(e.target[5].checked);
+    // console.log(e.target[6].checked);
+    // console.log(e.target[7].checked);
+    // console.log(e.target[8].checked);
   };
 
-  return (
-    <div className="container">
-      <div className="card card-primary">
-        <div className="card-header">
-          <h3 className="card-title">Pesan Ruangan</h3>
-        </div>
-        {/* /.card-header */}
-        {/* form start */}
-        <form onSubmit={handleSubmit}>
-          <div className="card-body">
-            <div className="form-group">
-              <label>Order ID</label>
-              <input type="text" className="form-control" id="orderID" />
-            </div>
-            <div className="form-group">
-              <label>Nama Pemesan</label>
-              <input
-                type="text"
-                className="form-control"
-                id="NamaPemesan"
-                placeholder="Nama Panjang"
-              />
-            </div>
-            <div className="form-group">
-              <label>Ruangan</label>
-              <select className="form-control">
-                <option>ROOM 001</option>
-                <option>ROOM 002</option>
-                <option>ROOM 003</option>
-                <option>ROOM 004</option>
-                <option>ROOM 005</option>
-              </select>
-            </div>
-
-            <div className="form-group">
-              <label>Tanggal Sewa:</label>
-              <div
-                className="input-group date"
-                id="reservationdate"
-                data-target-input="nearest"
-              >
+  render() {
+    return (
+      <div className="container">
+        <div className="card card-primary">
+          <div className="card-header">
+            <h3 className="card-title">Pesan Ruangan</h3>
+          </div>
+          {/* /.card-header */}
+          {/* form start */}
+          <form onSubmit={this.handleSubmit}>
+            <div className="card-body">
+              <div className="form-group">
+                <label>Order ID</label>
+                <input type="text" className="form-control" id="orderID" />
+              </div>
+              <div className="form-group">
+                <label>Nama Pemesan</label>
                 <input
                   type="text"
-                  className="form-control datetimepicker-input"
-                  data-target="#reservationdate"
-                  // onChange={(e) => setDate(e.target.value)}
-                  // onClick={(e) => console.log(e.target.value)}
-                  value={tanggalSekarang}
-                  onChange={(e) => console.log(e.target.value)}
+                  className="form-control"
+                  id="NamaPemesan"
+                  placeholder="Nama Panjang"
                 />
+              </div>
+              <div className="form-group">
+                <label>Ruangan</label>
+                <select className="form-control">
+                  <option>ROOM 001</option>
+                  <option>ROOM 002</option>
+                  <option>ROOM 003</option>
+                  <option>ROOM 004</option>
+                  <option>ROOM 005</option>
+                </select>
+              </div>
+
+              <div className="form-group">
+                <label>Tanggal Sewa:</label>
                 <div
-                  className="input-group-append"
-                  data-target="#reservationdate"
-                  data-toggle="datetimepicker"
+                  className="input-group date"
+                  id="reservationdate"
+                  data-target-input="nearest"
                 >
-                  <div className="input-group-text">
-                    <i className="fa fa-calendar" />
+                  <input
+                    type="text"
+                    className="form-control datetimepicker-input"
+                    data-target="#reservationdate"
+                    // onChange={(e) => setDate(e.target.value)}
+                    // onClick={(e) => console.log(e.target.value)}
+                    value={new Intl.DateTimeFormat("en-GB", {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                    }).format(this.state.tanggalSekarang)}
+                  ></input>
+                  <div
+                    className="input-group-append"
+                    data-target="#reservationdate"
+                    data-toggle="datetimepicker"
+                  >
+                    <div className="input-group-text">
+                      <i className="fa fa-calendar" />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="form-group">
-              <label>Tanggal Selesai</label>
-              <input
-                type="text"
-                className="form-control"
-                id="exampleInputPassword1"
-              />
-            </div>
-
-            <div className="form-group">
-              <label>Status</label>
-              <br />
-              <div
-                className="icheck-success d-inline"
-                style={{ marginRight: 10 }}
-              >
+              <div className="form-group">
+                <label>Tanggal Selesai</label>
                 <input
-                  type="radio"
-                  id="radioPrimary1"
-                  name="r1"
-                  defaultChecked
+                  type="text"
+                  className="form-control"
+                  id="exampleInputPassword1"
                 />
-                <label htmlFor="radioPrimary1">Active</label>
               </div>
-              <div
-                className="icheck-sunflower d-inline"
-                style={{ marginRight: 10 }}
-              >
-                <input type="radio" id="radioPrimary2" name="r1" />
-                <label htmlFor="radioPrimary2">Menunggu Pembayaran</label>
-              </div>
-              <div
-                className="icheck-concrete d-inline"
-                style={{ marginRight: 10 }}
-              >
-                <input type="radio" id="radioPrimary3" name="r1" />
-                <label htmlFor="radioPrimary3">Selesai</label>
-              </div>
-              <div className="icheck-danger d-inline">
-                <input type="radio" id="radioPrimary4" name="r1" />
-                <label htmlFor="radioPrimary4">Batal</label>
+
+              <div className="form-group">
+                <label>Status</label>
+                <br />
+                <div
+                  className="icheck-success d-inline"
+                  style={{ marginRight: 10 }}
+                >
+                  <input
+                    type="radio"
+                    id="radioPrimary1"
+                    name="r1"
+                    defaultChecked
+                  />
+                  <label htmlFor="radioPrimary1">Active</label>
+                </div>
+                <div
+                  className="icheck-sunflower d-inline"
+                  style={{ marginRight: 10 }}
+                >
+                  <input type="radio" id="radioPrimary2" name="r1" />
+                  <label htmlFor="radioPrimary2">Menunggu Pembayaran</label>
+                </div>
+                <div
+                  className="icheck-concrete d-inline"
+                  style={{ marginRight: 10 }}
+                >
+                  <input type="radio" id="radioPrimary3" name="r1" />
+                  <label htmlFor="radioPrimary3">Selesai</label>
+                </div>
+                <div className="icheck-danger d-inline">
+                  <input type="radio" id="radioPrimary4" name="r1" />
+                  <label htmlFor="radioPrimary4">Batal</label>
+                </div>
               </div>
             </div>
-          </div>
-          {/* /.card-body */}
-          <div className="card-footer">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-        </form>
+            {/* /.card-body */}
+            <div className="card-footer">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default PesanRuangan;
