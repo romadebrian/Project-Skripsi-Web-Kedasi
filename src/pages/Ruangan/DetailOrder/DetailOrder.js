8122e155239d41cc72c4350296cf8f004e1e25d7
romadebrian <roma_coll04@yahoo.com>
2021-12-23 00:35:13 +0700

    Add page detail order

--- src/pages/Ruangan/DetailOrder/DetailOrder.js
+++ src/pages/Ruangan/DetailOrder/DetailOrder.js
@@ -1,8 +1,109 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firebase from "../../../config/firebase";
 
 function DetailOrder() {
+  const [tanggalSekarang, setTanggalSekarang] = useState("");
+  const [isloaded, setLoaded] = useState(false);
+  // const [statusPembayaran, setStatusPembayaran] = useState("");
+  // const [tampilModal, setTampilModal] = useState(true);
+
+  useEffect(() => {
+    window.$("#TanggalSewa").datetimepicker({
+      format: "DD-MM-YYYY",
+    });
+    window.$("#TanggalSelesai").datetimepicker({
+      format: "DD-MM-YYYY",
+    });
+
+    function convertTanggalSekarang() {
+      let hariini = new Date();
+
+      let tgl =
+        hariini.getDate() +
+        "-" +
+        parseInt(hariini.getMonth() + 1) +
+        "-" +
+        hariini.getFullYear();
+
+      setTanggalSekarang(tgl);
+    }
+
+    if (isloaded === false) {
+      convertTanggalSekarang();
+
+      console.log(new Date());
+
+      setLoaded(true);
+    }
+  }, [isloaded]);
+
+  const handleSubmit = async (e) => {
+    var StatusPembayaran;
+    e.preventDefault();
+
+    console.log("Order Id: ", e.target[0].value);
+    console.log("Nama Pemesan: ", e.target[1].value);
+    console.log("Ruangan: ", e.target[2].value);
+    console.log("Tanggal Sewa: ", e.target[3].value);
+    console.log("Tanggal Selesai: ", e.target[4].value);
+
+    if (e.target[5].checked === true) {
+      StatusPembayaran = "Active";
+      console.log("Active");
+    } else if (e.target[6].checked === true) {
+      StatusPembayaran = "Menunggu Pembayaran";
+      console.log("Menunggu Pembayaran");
+    } else if (e.target[7].checked === true) {
+      StatusPembayaran = "Selesai";
+      console.log("Selesai");
+    } else if (e.target[8].checked === true) {
+      StatusPembayaran = "Batal";
+      console.log("Batal");
+    } else {
+      console.log("error");
+    }
+
+    firebase
+      .database()
+      .ref("order")
+      .push(
+        {
+          OrderId: e.target[0].value,
+          NamaPemesan: e.target[1].value,
+          Ruangan: e.target[2].value,
+          TanggalSewa: e.target[3].value,
+          TanggalSelesai: e.target[4].value,
+          Status: StatusPembayaran,
+        },
+        (error) => {
+          if (error) {
+            // The write failed...
+            alert("Gagal Simpan");
+          } else {
+            // Data saved successfully!
+            alert("Profile Berhasil Di Simpan");
+            console.log(
+              "send value: ",
+              e.target[0].value,
+              e.target[1].value,
+              e.target[2].value,
+              e.target[3].value,
+              e.target[4].value,
+              StatusPembayaran
+            );
+            window.location.reload();
+
+            // window.$(this.modal).modal("hide");
+            // window.$(this.modal).on("hidden.bs.modal");
+
+            // e.target[10].dismiss = "modal";
+          }
+        }
+      );
+  };
+
   return (
-    <div className="modal fade" id="modal-lg">
+    <div className="modal fade" id="form-edit">
       <div className="modal-dialog modal-lg">
         <div className="modal-content">
           <div className="card card-primary">
