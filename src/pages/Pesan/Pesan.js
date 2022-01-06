f97447a03706addb642ecad15eec3a6b70b84b09
romadebrian <roma_coll04@yahoo.com>
2022-01-06 23:26:05 +0700

    function to get list suer

--- src/pages/Pesan/Pesan.js
+++ src/pages/Pesan/Pesan.js
@@ -1,49 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import ChatBox from "./ChatBox/ChatBox";
 import "./Pesan.css";
 import ItemUserChat from "./props/ItemUserChat";
-function Pesan() {
-  return (
-    <div className="chatbox">
-      {/* List User Who Messege You */}
-      <div
-        className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white"
-        // style={{ width: 380, height: 640 }}
-      >
-        <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-          <span className="fs-5 fw-semibold select-user">Pilih User</span>
+import firebase from "../../config/firebase";
 
-          <select className="form-control" id="Frm_Chat_User">
-            <option></option>
-            <option>User 1</option>
-            <option>User 2</option>
-            <option>User 3</option>
-            <option>User 5</option>
-            <option>User 6</option>
-          </select>
+class Pesan extends Component {
+  componentDidMount() {
+    this.handleGetListUser();
+  }
+
+  handleGetListUser = (nextPageToken) => {
+    // getAuth();
+
+    // List batch of users, 1000 at a time.
+    getAuth()
+      .listUsers(1000, nextPageToken)
+      .then((listUsersResult) => {
+        listUsersResult.users.forEach((userRecord) => {
+          console.log("user", userRecord.toJSON());
+        });
+        if (listUsersResult.pageToken) {
+          // List next batch of users.
+          listAllUsers(listUsersResult.pageToken);
+        }
+      })
+      .catch((error) => {
+        console.log("Error listing users:", error);
+      });
+  };
+
+  render() {
+    return (
+      <div className="chatbox">
+        {/* List User Who Messege You */}
+        <div
+          className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white"
+          // style={{ width: 380, height: 640 }}
+        >
+          <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+            <span className="fs-5 fw-semibold select-user">Pilih User</span>
+
+            <select className="form-control" id="Frm_Chat_User">
+              <option></option>
+              <option>User 1</option>
+              <option>User 2</option>
+              <option>User 3</option>
+              <option>User 5</option>
+              <option>User 6</option>
+            </select>
+          </div>
+          <div className="list-group list-group-flush border-bottom scrollarea">
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+            <ItemUserChat />
+          </div>
         </div>
-        <div className="list-group list-group-flush border-bottom scrollarea">
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-          <ItemUserChat />
-        </div>
+
+        {/* End Part List User Who Messege You */}
+
+        {/* Chat Box */}
+        <ChatBox />
+        {/* End of Chat Box */}
       </div>
-
-      {/* End Part List User Who Messege You */}
-
-      {/* Chat Box */}
-      <ChatBox />
-      {/* End of Chat Box */}
-    </div>
-  );
+    );
+  }
 }
 
 export default Pesan;
