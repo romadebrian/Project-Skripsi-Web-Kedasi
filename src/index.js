37820d7c5f90d8f161a1dda898f5deb6e1a6592a
romadebrian <roma_coll04@yahoo.com>
2022-04-08 00:07:57 +0700

    clean redux setup

--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { onAuthStateChanged } from "firebase/auth";
-import { getAuth } from "./config/firebase";
-import { storeRedux } from "./config/redux/redux";
-// import appFirebase from "./config/firebase";
 
-// console.log(appFirebase);
-onAuthStateChanged(getAuth, (currentUser) => {
-  storeRedux.dispatch({ type: "SET_USER", userData: currentUser });
-  ReactDOM.render(<App />, document.getElementById("root"));
-});
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
