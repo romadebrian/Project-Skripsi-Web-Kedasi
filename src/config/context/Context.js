f2502205dcee9397db655b47495c554bc4f2e84b
romadebrian <roma_coll04@yahoo.com>
2022-04-11 22:26:39 +0700

    setup context API

---
+++ src/config/context/Context.js
@@ -0,0 +1,53 @@
+import { onAuthStateChanged } from "firebase/auth";
+import {
+  Component,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { getAuth } from "../firebase";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = (props) => {
+  const [user, setUser] = useState("loading");
+  // const [error, setError] = useState(null);
+  console.log("datauser firebase", user);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth, setUser);
+    return () => unsubscribe;
+  }, []);
+
+  return <AuthContext.Provider value={user} {...props} />;
+};
+
+export const useAuthState = () => {
+  const auth = useContext(AuthContext);
+  // console.log("test", auth);
+
+  return { ...auth, isAuthenticated: auth != null };
+};
+
+// export const DataCurrentUser = () => {
+//   const dataUser = useContext(AuthContext);
+//   console.log("test", dataUser);
+
+//   return { ...dataUser, UserData: dataUser };
+// };
+
+const Cosumer = AuthContext.Consumer;
+export const DataCurrentUser = (Childern) => {
+  return class ParentCosumer extends Component {
+    render() {
+      return (
+        <Cosumer>
+          {(value) => {
+            return <Childern {...this.props} {...value} />;
+          }}
+        </Cosumer>
+      );
+    }
+  };
+};
