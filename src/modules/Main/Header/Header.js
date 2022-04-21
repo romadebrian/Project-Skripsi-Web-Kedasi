bf2c92ae777bad21e83a04280e3b6353f68853e4
romadebrian <roma_coll04@yahoo.com>
2022-04-21 17:14:59 +0700

    function to get deferent hour and days

--- src/modules/Main/Header/Header.js
+++ src/modules/Main/Header/Header.js
@@ -11,8 +11,6 @@ class Header extends Component {
 
   componentDidMount() {
     this.handleGetDataNotification();
-
-    this.getDeferentTime();
   }
 
   componentDidUpdate() {
@@ -41,20 +39,31 @@ class Header extends Component {
           console.log("Data tidak ditemukan");
         }
 
-        this.setState({ listNotifikasi: data });
+        this.setState({ listNotifikasi: data }, () => {
+          this.getDeferentTime();
+        });
 
         console.log("List Notification in header: ", this.state.listNotifikasi);
       });
   };
 
   getDeferentTime = () => {
-    var date1 = new Date("06/30/2019");
-    var date2 = new Date("07/30/2019");
+    var date1 = new Date(this.state.listNotifikasi[5].data.waktu);
+    var date2 = new Date();
 
     var Difference_In_Time = date2.getTime() - date1.getTime();
-    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
 
-    console.log("Deferent Time", Difference_In_Days);
+    var Difference_In_Hours = Math.round(Difference_In_Time / (1000 * 3600));
+
+    var Difference_In_Days = Math.round(
+      Difference_In_Time / (1000 * 3600 * 24)
+    );
+
+    console.log("Tanggal Notifikasi", date1);
+    console.log("Tanggal Sekarang", date2);
+
+    console.log("Deferent Hours", Difference_In_Hours);
+    // console.log("Deferent Days", Difference_In_Days);
   };
 
   render() {
