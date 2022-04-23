1c0e1385aa2cf0a92f9a2cb6a4290d49312b808e
romadebrian <roma_coll04@yahoo.com>
2022-04-23 16:44:51 +0700

    apply value of history chat in header chat

--- src/modules/Main/Header/Header.js
+++ src/modules/Main/Header/Header.js
@@ -221,14 +221,14 @@ class Header extends Component {
           {/* Left navbar links */}
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a
+              <Link
                 className="nav-link"
                 data-widget="pushmenu"
-                href="/"
+                to="#"
                 role="button"
               >
                 <i className="fas fa-bars" />
-              </a>
+              </Link>
             </li>
             <li className="nav-item d-none d-sm-inline-block">
               <Link to="/" className="nav-link">
@@ -240,41 +240,17 @@ class Header extends Component {
           <ul className="navbar-nav ml-auto">
             {/* Messages Dropdown Menu */}
             <li className="nav-item dropdown">
-              <a className="nav-link" data-toggle="dropdown" href="/">
+              <Link className="nav-link" data-toggle="dropdown" to="#">
                 <i className="far fa-comments" />
                 <span className="badge badge-danger navbar-badge">3</span>
-              </a>
+              </Link>
               <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <a href="/" className="dropdown-item">
-                  {/* Message Start */}
-                  <div className="media">
-                    <img
-                      src="dist/img/user2-160x160.jpg"
-                      alt="User Avatar"
-                      className="img-size-50 mr-3 img-circle"
-                    />
-                    <div className="media-body">
-                      <h3 className="dropdown-item-title">
-                        Brad Diesel
-                        <span className="float-right text-sm text-danger">
-                          <i className="fas fa-star" />
-                        </span>
-                      </h3>
-                      <p className="text-sm">Call me whenever you can...</p>
-                      <p className="text-sm text-muted">
-                        <i className="far fa-clock mr-1" /> 4 Hours Ago
-                      </p>
-                    </div>
-                  </div>
-                  {/* Message End */}
-                </a>
-                <div className="dropdown-divider" />
-
                 {this.state.dataHistoryChat.length > 0 ? (
                   <Fragment>
                     {this.state.dataHistoryChat.map((chat) => {
-                      // console.log("data yang di render", chat);
-                      // console.log("data yang di render", chat.dataSusun);
+                      const elapsedTimeChat = this.getDeferentTime(
+                        chat.tanggalLastChat
+                      );
 
                       return (
                         // <ItemUserChat
@@ -287,30 +263,29 @@ class Header extends Component {
                         //   ActionClick={(e) => this.handleClickItemUserChat(e)}
                         // />
                         <Fragment key={chat.id}>
-                          <a href="/" className="dropdown-item">
+                          <Link to="/pesan" className="dropdown-item">
                             {/* Message Start */}
                             <div className="media">
                               <img
-                                src="/dist/img/romadebrian.png"
+                                src={chat.photo}
                                 alt="User Avatar"
                                 className="img-size-50 img-circle mr-3"
+                                style={{ height: "50px" }}
                               />
                               <div className="media-body">
                                 <h3 className="dropdown-item-title">
-                                  John Pierce
+                                  {chat.nama}
                                   <span className="float-right text-sm text-muted"></span>
                                 </h3>
-                                <p className="text-sm">
-                                  I got your message bro
-                                </p>
+                                <p className="text-sm">{chat.lastChat}</p>
                                 <p className="text-sm text-muted">
-                                  <i className="far fa-clock mr-1" /> 4 Hours
-                                  Ago
+                                  <i className="far fa-clock mr-1" />
+                                  {elapsedTimeChat}
                                 </p>
                               </div>
                             </div>
                             {/* Message End */}
-                          </a>
+                          </Link>
                           <div className="dropdown-divider" />
                         </Fragment>
                       );
@@ -318,28 +293,6 @@ class Header extends Component {
                   </Fragment>
                 ) : null}
 
-                <a href="/" className="dropdown-item">
-                  {/* Message Start */}
-                  <div className="media">
-                    <img
-                      src="dist/img/user2-160x160.jpg"
-                      alt="User Avatar"
-                      className="img-size-50 img-circle mr-3"
-                    />
-                    <div className="media-body">
-                      <h3 className="dropdown-item-title">
-                        Nora Silvester
-                        <span className="float-right text-sm text-warning"></span>
-                      </h3>
-                      <p className="text-sm">The subject goes here</p>
-                      <p className="text-sm text-muted">
-                        <i className="far fa-clock mr-1" /> 4 Hours Ago
-                      </p>
-                    </div>
-                  </div>
-                  {/* Message End */}
-                </a>
-                <div className="dropdown-divider" />
                 <a href="/" className="dropdown-item dropdown-footer">
                   See All Messages
                 </a>
