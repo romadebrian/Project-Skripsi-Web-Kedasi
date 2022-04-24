0705be4f41e9f3f94b469a7f565c25fb4ee98600
romadebrian <roma_coll04@yahoo.com>
2022-04-25 03:10:14 +0700

    base edit array

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,105 +1,181 @@
-function Home() {
-  return (
-    <div>
-      {/* Content Header (Page header) */}
-      <div className="content-header">
-        <div className="container-fluid">
-          <div className="row mb-2">
-            <div className="col-sm-6">
-              <h1 className="m-0">Dashboard</h1>
-            </div>
-            {/* /.col */}
-            <div className="col-sm-6">
-              <ol className="breadcrumb float-sm-right">
-                <li className="breadcrumb-item">
-                  <a href="/">Home</a>
-                </li>
-                <li className="breadcrumb-item active">Dashboard v1</li>
-              </ol>
-            </div>
-            {/* /.col */}
-          </div>
-          {/* /.row */}
-        </div>
-        {/* /.container-fluid */}
-      </div>
-      {/* /.content-header */}
+import { Component } from "react";
+import firebase from "../../config/firebase";
 
-      {/* Main content */}
-      <section className="content">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-lg-3 col-6">
-              {/* small box */}
-              <div className="small-box bg-info">
-                <div className="inner">
-                  <h3>5</h3>
-                  <p>New Orders</p>
-                </div>
-                <div className="icon">
-                  <i className="ion ion-bag" />
-                </div>
-                <a href="/" className="small-box-footer">
-                  More info <i className="fas fa-arrow-circle-right" />
-                </a>
+class Home extends Component {
+  state = {
+    listOrder: [],
+    totalStatus: [
+      { totalActive: 0, totalPending: 0, totalDone: 0, totalCancel: 0 },
+    ],
+  };
+
+  componentDidMount() {
+    this.handleGetData();
+  }
+
+  handleGetData = () => {
+    return firebase
+      .database()
+      .ref("/order/")
+      .on("value", (snapshot) => {
+        const data = [];
+        if (snapshot.exists()) {
+          Object.keys(snapshot.val()).map((key) => {
+            data.push({
+              id: key,
+              data: snapshot.val()[key],
+            });
+            return data;
+          });
+        } else {
+          console.log("Data tidak ditemukan");
+        }
+
+        this.setState({ listOrder: data }, () => {
+          console.log("List Order: ", data);
+          this.handleTotalOrderStatus();
+        });
+      });
+  };
+
+  handleTotalOrderStatus = () => {
+    console.log(this.state.listOrder[1].data.Status);
+    let totalOrder = this.state.listOrder.length;
+
+    console.log(totalOrder);
+
+    let Active = 0;
+    let Pennding = 0;
+    let Done = 0;
+    let Cancel = 0;
+
+    const checkStatus = (i) => {
+      if (this.state.listOrder[i].data.Status === "Active") {
+        Active = Active + 1;
+        this.setState(
+          {
+            totalStatus: [
+              {
+                ...this.state.totalStatus[0],
+                totalActive: Active,
+              },
+            ],
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      }
+    };
+
+    let i = 0;
+    do {
+      checkStatus(i);
+      i++;
+    } while (i < totalOrder);
+  };
+
+  render() {
+    return (
+      <div>
+        {/* Content Header (Page header) */}
+        <div className="content-header">
+          <div className="container-fluid">
+            <div className="row mb-2">
+              <div className="col-sm-6">
+                <h1 className="m-0">Dashboard</h1>
               </div>
-            </div>
-            {/* ./col */}
-            <div className="col-lg-3 col-6">
-              {/* small box */}
-              <div className="small-box bg-success">
-                <div className="inner">
-                  <h3>5</h3>
-                  <p>Room Active</p>
-                </div>
-                <div className="icon">
-                  <i className="ion ion-stats-bars" />
-                </div>
-                <a href="/" className="small-box-footer">
-                  More info <i className="fas fa-arrow-circle-right" />
-                </a>
+              {/* /.col */}
+              <div className="col-sm-6">
+                <ol className="breadcrumb float-sm-right">
+                  <li className="breadcrumb-item">
+                    <a href="/">Home</a>
+                  </li>
+                  <li className="breadcrumb-item active">Dashboard v1</li>
+                </ol>
               </div>
+              {/* /.col */}
             </div>
-            {/* ./col */}
-            <div className="col-lg-3 col-6">
-              {/* small box */}
-              <div className="small-box bg-warning">
-                <div className="inner">
-                  <h3>3</h3>
-                  <p>Unpaid Order</p>
-                </div>
-                <div className="icon">
-                  <i className="ion ion-person-add" />
-                </div>
-                <a href="/" className="small-box-footer">
-                  More info <i className="fas fa-arrow-circle-right" />
-                </a>
-              </div>
-            </div>
-            {/* ./col */}
-            <div className="col-lg-3 col-6">
-              {/* small box */}
-              <div className="small-box bg-danger">
-                <div className="inner">
-                  <h3>10</h3>
-                  <p>
-                    Total Order <br /> On This Month
-                  </p>
-                </div>
-                <div className="icon">
-                  <i className="ion ion-pie-graph" />
-                </div>
-                <a href="/" className="small-box-footer">
-                  More info <i className="fas fa-arrow-circle-right" />
-                </a>
-              </div>
-            </div>
-            {/* ./col */}
+            {/* /.row */}
           </div>
+          {/* /.container-fluid */}
         </div>
-      </section>
-    </div>
-  );
+        {/* /.content-header */}
+
+        {/* Main content */}
+        <section className="content">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-3 col-6">
+                {/* small box */}
+                <div className="small-box bg-success">
+                  <div className="inner">
+                    <h3>{this.state.totalStatus[0].totalActive}</h3>
+                    <p>Orders Active</p>
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-bag" />
+                  </div>
+                  <a href="/" className="small-box-footer">
+                    More info <i className="fas fa-arrow-circle-right" />
+                  </a>
+                </div>
+              </div>
+              {/* ./col */}
+              <div className="col-lg-3 col-6">
+                {/* small box */}
+                <div className="small-box bg-warning">
+                  <div className="inner">
+                    <h3>5</h3>
+                    <p>Unpaid Order</p>
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-stats-bars" />
+                  </div>
+                  <a href="/" className="small-box-footer">
+                    More info <i className="fas fa-arrow-circle-right" />
+                  </a>
+                </div>
+              </div>
+              {/* ./col */}
+              <div className="col-lg-3 col-6">
+                {/* small box */}
+                <div className="small-box bg-secondary">
+                  <div className="inner">
+                    <h3>3</h3>
+                    <p>Order Done</p>
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-person-add" />
+                  </div>
+                  <a href="/" className="small-box-footer">
+                    More info <i className="fas fa-arrow-circle-right" />
+                  </a>
+                </div>
+              </div>
+              {/* ./col */}
+              <div className="col-lg-3 col-6">
+                {/* small box */}
+                <div className="small-box bg-danger">
+                  <div className="inner">
+                    <h3>10</h3>
+                    <p>Order Cancel</p>
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-pie-graph" />
+                  </div>
+                  <a href="/" className="small-box-footer">
+                    More info <i className="fas fa-arrow-circle-right" />
+                  </a>
+                </div>
+              </div>
+              {/* ./col */}
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Home;
