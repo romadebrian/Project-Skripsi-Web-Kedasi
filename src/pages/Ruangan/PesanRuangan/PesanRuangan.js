92d520a7e85fac361fd9cc3c625aeecc21ac1796
romadebrian <roma_coll04@yahoo.com>
2022-05-17 17:51:33 +0700

    prototype check between date

--- src/pages/Ruangan/PesanRuangan/PesanRuangan.js
+++ src/pages/Ruangan/PesanRuangan/PesanRuangan.js
@@ -174,6 +174,20 @@ function PesanRuangan(props) {
 
         console.log(dataHasil);
         // this.setState({ orderDetail: dataHasil[0].data });
+
+        var dateFrom = "02/05/2013";
+        var dateTo = "02/09/2013";
+        var dateCheck = "02/01/2013";
+
+        var d1 = dateFrom.split("/");
+        var d2 = dateTo.split("/");
+        var c = dateCheck.split("/");
+
+        var from = new Date(d1[2], parseInt(d1[1]) - 1, d1[0]); // -1 because months are from 0 to 11
+        var to = new Date(d2[2], parseInt(d2[1]) - 1, d2[0]);
+        var check = new Date(c[2], parseInt(c[1]) - 1, c[0]);
+
+        console.log(check > from && check < to);
       });
   };
 
