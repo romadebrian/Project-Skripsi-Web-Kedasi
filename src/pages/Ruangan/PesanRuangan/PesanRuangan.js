943b7082ea38fed236dc75f767132b365325e692
romadebrian <roma_coll04@yahoo.com>
2022-05-19 03:12:56 +0700

    single check avaliable date

--- src/pages/Ruangan/PesanRuangan/PesanRuangan.js
+++ src/pages/Ruangan/PesanRuangan/PesanRuangan.js
@@ -173,21 +173,41 @@ function PesanRuangan(props) {
         }
 
         console.log(dataHasil);
+        console.log(dataHasil[0].data.TanggalSewa);
+        console.log(dataHasil[0].data.TanggalSelesai);
+        console.log(tglMulai);
         // this.setState({ orderDetail: dataHasil[0].data });
 
-        var dateFrom = "02/05/2013";
-        var dateTo = "02/09/2013";
-        var dateCheck = "02/01/2013";
+        // Formating Selected Date
 
-        var d1 = dateFrom.split("/");
-        var d2 = dateTo.split("/");
-        var c = dateCheck.split("/");
+        let hariini = tglMulai;
+
+        let tgl =
+          hariini.getDate() +
+          "-" +
+          parseInt(hariini.getMonth() + 1) +
+          "-" +
+          hariini.getFullYear();
+
+        console.log(tgl);
+
+        var dateFrom = dataHasil[0].data.TanggalSewa;
+        var dateTo = dataHasil[0].data.TanggalSelesai;
+        var dateCheck = tgl;
+
+        var d1 = dateFrom.split("-");
+        var d2 = dateTo.split("-");
+        var c = dateCheck.split("-");
+
+        // console.log(d1);
+        // console.log(d2);
+        // console.log(c);
 
         var from = new Date(d1[2], parseInt(d1[1]) - 1, d1[0]); // -1 because months are from 0 to 11
         var to = new Date(d2[2], parseInt(d2[1]) - 1, d2[0]);
         var check = new Date(c[2], parseInt(c[1]) - 1, c[0]);
 
-        console.log(check > from && check < to);
+        console.log(check >= from && check <= to);
       });
   };
 
@@ -327,12 +347,12 @@ function PesanRuangan(props) {
                     /> */}
                     <DateTimePicker
                       className="form-control "
-                      onChange={(e) => {
-                        setTglMulai(e);
-                        console.log(e);
-                      }}
                       value={tglMulai}
                       format={"dd-MM-y"}
+                      onChange={(e) => {
+                        setTglMulai(e);
+                        // console.log(e);
+                      }}
                     />
                     <div className="input-group-append">
                       <div
