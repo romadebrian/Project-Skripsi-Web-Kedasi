1e62ded7f8d84e41c7a1026c5c4d964b44d87ae8
romadebrian <roma_coll04@yahoo.com>
2022-12-16 13:54:27 +0700

    ganti nama fungsi ke Main

--- src/modules/Main/main.js
+++ src/modules/Main/main.js
@@ -22,7 +22,7 @@ import InvoicePrint from "../../pages/Ruangan/DetailOrder/InvoicePrint";
 // import PrintLaporan from "../../pages/Ruangan/props/Ruangan/PrintLaporan";
 
 // Fungsi untuk yang di tampilkan ke index.html
-function App(props) {
+function Main(props) {
   let location = useLocation();
   // const history = useHistory();
 
@@ -81,4 +81,4 @@ function App(props) {
   );
 }
 
-export default withRouter(App);
+export default withRouter(Main);
