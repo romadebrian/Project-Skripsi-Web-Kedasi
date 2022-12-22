0fc60c453f3d09ad21df66d50368d6d61740a6dc
romadebrian <roma_coll04@yahoo.com>
2022-12-22 15:32:26 +0700

    membuat notifikasi untuk satu user detail order

--- src/pages/Notifikasi/CreateNotification/CreateNotification.js
+++ src/pages/Notifikasi/CreateNotification/CreateNotification.js
@@ -3,9 +3,17 @@ import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 import firebase from "../../../config/firebase";
 
+import { FormattingDateTime } from "../../../config/formattingDateTime";
+
 function CreateNotification(props) {
   const [isloaded, setLoaded] = useState(false);
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [user, setUser] = useState("All");
   const [aksi, setAksi] = useState("");
+  const [orderId, setOrderID] = useState("");
+
+  const [listOrderUser, setListOrderUser] = useState(["ORD001", "ORD002"]);
 
   useEffect(() => {
     window.$("#TanggalSewa").datetimepicker({
@@ -21,60 +29,164 @@ function CreateNotification(props) {
   }, [isloaded]);
 
   const handleSubmit = (e) => {
-    var aksinya;
-    var tanggal = new Date().toUTCString();
     e.preventDefault();
 
-    console.log("Judul: ", e.target[0].value);
-    console.log("isi: ", e.target[1].value);
-    console.log("Target: ", e.target[2].value);
+    console.log("Judul: ", title);
+    console.log("isi: ", message);
+    console.log("Target: ", user);
 
-    if (e.target[3].value === "Default") {
-      aksinya = "Default";
-      console.log("Default");
-    } else if (e.target[3].value === "Buka Aplikasi") {
-      aksinya = e.target[4].value;
-      console.log("Buka Aplikasi", e.target[4].value);
-    } else if (e.target[3].value === "Buka Url") {
-      aksinya = e.target[4].value;
-      console.log("Buka Url", e.target[4].value);
+    if (user === "All") {
+      handleCreateNotificationToAll();
     } else {
-      console.log("error");
+      handleCreateNotificationToOneUser();
     }
 
-    firebase
-      .database()
-      .ref("notifikasi")
-      .push(
-        {
-          Judul: e.target[0].value,
-          Isi: e.target[1].value,
-          Target: e.target[2].value,
-          Aksi: aksinya,
-          waktu: tanggal,
-          Status: "Unread",
-        },
-        (error) => {
-          if (error) {
-            // The write failed...
-            alert("Gagal Simpan");
-          } else {
-            // Data saved successfully!
-            // alert("Profile Berhasil Di Simpan");
-            toastSucces();
-            console.log(
-              "send value: ",
-              e.target[0].value,
-              e.target[1].value,
-              e.target[2].value,
-              aksinya,
-              "Unread"
-            );
+    // if (e.target[3].value === "Default") {
+    //   aksinya = "Default";
+    //   console.log("Default");
+    // } else if (e.target[3].value === "Buka Aplikasi") {
+    //   aksinya = e.target[4].value;
+    //   console.log("Buka Aplikasi", e.target[4].value);
+    // } else if (e.target[3].value === "Buka Url") {
+    //   aksinya = e.target[4].value;
+    //   console.log("Buka Url", e.target[4].value);
+    // } else {
+    //   console.log("error");
+    // }
 
-            window.$("#form-notifikasi").modal("hide");
-          }
+    // firebase
+    //   .database()
+    //   .ref("notifikasi")
+    //   .push(
+    //     {
+    //       Judul: e.target[0].value,
+    //       Isi: e.target[1].value,
+    //       Target: e.target[2].value,
+    //       Aksi: aksinya,
+    //       waktu: tanggal,
+    //       Status: "Unread",
+    //     },
+    //     (error) => {
+    //       if (error) {
+    //         // The write failed...
+    //         alert("Gagal Simpan");
+    //       } else {
+    //         // Data saved successfully!
+    //         // alert("Profile Berhasil Di Simpan");
+    //         toastSucces();
+    //         console.log(
+    //           "send value: ",
+    //           e.target[0].value,
+    //           e.target[1].value,
+    //           e.target[2].value,
+    //           aksinya,
+    //           "Unread"
+    //         );
+
+    //         window.$("#form-notifikasi").modal("hide");
+    //       }
+    //     }
+    //   );
+  };
+
+  const handleCreateNotificationToOneUser = (e) => {
+    // var idUser = e.target[2].value;
+    var idUser = "zAhbiHR06ZQbwSdTiT6ftB91BH62";
+    var to =
+      "dCupw9lMSuCPzo_d7PKjxh:APA91bFfOr4piBZ-16ac9XIGO-gHcOIJEyp_7QyMWKad61dpZ5lbNlK5NjC77gaBnuB6N1oV3dLpXt6uQPYxGZ1F7ZlmaVxeV6L43Mm1ZTKvUUIYGR9964jfTLcac49r_aP7Z_cIwNWe";
+    var DateTimeNow = FormattingDateTime(new Date());
+
+    // console.log(DateTimeNow);
+
+    if (aksi === "Transaksi") {
+      const data = { Action: "CheckOut", OrderID: orderId };
+      handleCreateRemoteNotification(to, data);
+    } else if (aksi === "Pemberitahuan") {
+      const data = { Action: "Notification" };
+      handleCreateRemoteNotification(to, data);
+    }
+
+    var postListRef = firebase.database().ref(`users/${idUser}/notifikasi`);
+    var newPostRef = postListRef.push();
+    newPostRef.set(
+      {
+        Aksi: aksi,
+        Date: DateTimeNow,
+        Isi: message,
+        Judul: title,
+        Meta_Data: orderId,
+        Status: "Unread",
+        Target: idUser,
+      },
+      (error) => {
+        if (error) {
+          alert("Gagal Simpan");
+        } else {
+          toastSucces();
+          clearValue();
+          window.$("#form-notifikasi").modal("hide");
         }
-      );
+      }
+    );
+
+    // const db = getDatabase();
+    // const addNotification = ref(db, `notifikasi`);
+    // const newNotificationRef = push(addNotification);
+
+    // set(newNotificationRef, {
+    //   Aksi: aksi,
+    //   Date: DateTimeNow,
+    //   Isi: e.target[1].value,
+    //   Judul: e.target[0].value,
+    //   Meta_Data: metaData,
+    //   Status: "Unread",
+    //   Target: idUser,
+    // });
+  };
+
+  const handleCreateNotificationToAll = (e) => {
+    // var DateTimeNow = FormattingDateTime(new Date());
+    // console.log(DateTimeNow);
+    // var postListRef = firebase.database().ref(`users/${idUser}/notifikasi`);
+    // var newPostRef = postListRef.push();
+    // newPostRef.set({
+    //   Aksi: aksi,
+    //   Date: DateTimeNow,
+    //   Isi: e.target[1].value,
+    //   Judul: e.target[0].value,
+    //   Meta_Data: metaData,
+    //   Status: "Unread",
+    //   Target: idUser,
+    // });
+  };
+
+  const handleCreateRemoteNotification = async (to, data) => {
+    console.log(data);
+    const myData = {
+      to: to,
+      priority: "high",
+      notification: {
+        title: title,
+        body: message,
+      },
+      // data: { Action: "Chat" },
+      // data: { Action: "CheckOut", OrderID: "ORD0047" },
+      data: data,
+      // foreground: true
+    };
+
+    const result = await fetch("https://fcm.googleapis.com/fcm/send", {
+      method: "POST",
+      headers: {
+        Authorization:
+          "key=AAAA6jj0k_w:APA91bFVagvVrQ1UsvzH-GglbdFAzvfuGhE1A6KABx3Y3QdiiyKNba9RG6zAkYqm3oAd23M-l7BuhzatGHAOHln6L2lho1ZrhMUM5DB678r2Z9_Bd79z46HCiezO9q9zD6CaiTa_h6C2",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(myData),
+    });
+
+    const resultJson = await result.json();
+    console.log(resultJson);
   };
 
   const toastSucces = () => {
@@ -91,8 +203,10 @@ function CreateNotification(props) {
     });
   };
 
-  const handleAksi = (params) => {
-    setAksi(params.target.value);
+  const clearValue = () => {
+    setTitle("");
+    setMessage("");
+    setUser("All");
   };
 
   return (
@@ -112,8 +226,9 @@ function CreateNotification(props) {
                   <input
                     type="text"
                     className="form-control"
-                    id="Frm_Judul"
                     placeholder="judul notifikasi"
+                    value={title}
+                    onChange={({ target }) => setTitle(target.value)}
                   />
                 </div>
                 <div className="form-group">
@@ -122,14 +237,20 @@ function CreateNotification(props) {
                     type="text"
                     className="form-control"
                     rows="3"
-                    id="Frm_Isi"
                     placeholder="isi notifikasi"
+                    value={message}
+                    onChange={({ target }) => setMessage(target.value)}
                   />
                 </div>
 
                 <div className="form-group">
                   <label>Target</label>
-                  <select className="form-control" id="Frm_Target_User">
+                  <select
+                    className="form-control"
+                    id="Frm_Target_User"
+                    value={user}
+                    onChange={({ target }) => setUser(target.value)}
+                  >
                     <option>All</option>
                     <option>User 1</option>
                     <option>User 2</option>
@@ -144,33 +265,29 @@ function CreateNotification(props) {
                   <select
                     className="form-control"
                     id="Frm_Aksi"
-                    onChange={(e) => handleAksi(e)}
+                    onChange={({ target }) => setAksi(target.value)}
                   >
-                    <option>Default</option>
-                    <option>Buka Aplikasi</option>
-                    <option>Buka Url</option>
+                    <option>Pemberitahuan</option>
+                    {user === "All" ? null : <option>Transaksi</option>}
                   </select>
                 </div>
 
-                {aksi === "Buka Aplikasi" ? (
+                {aksi === "Transaksi" ? (
                   <div className="form-group">
-                    <label>Target Aplikasi</label>
-                    <select className="form-control" id="Frm_Target_Aplikasi">
-                      <option>Halaman Utama</option>
-                      <option>Pesan Ruangan</option>
-                      <option>Chat</option>
-                      <option>Notifikasi Area</option>
-                    </select>
-                  </div>
-                ) : aksi === "Buka Url" ? (
-                  <div className="form-group">
-                    <label>Link</label>
-                    <input
-                      type="text"
+                    {/* <input /> */}
+                    <label>ID Order</label>
+                    <select
                       className="form-control"
-                      id="FrmLlink-Url"
-                      placeholder="http://"
-                    />
+                      onChange={({ target }) => setOrderID(target.value)}
+                    >
+                      {listOrderUser.length > 0 ? (
+                        listOrderUser.map((IdOrder) => {
+                          return <option key={IdOrder}>{IdOrder}</option>;
+                        })
+                      ) : (
+                        <option>User Belum Memiliki Transaksi</option>
+                      )}
+                    </select>
                   </div>
                 ) : null}
               </div>
