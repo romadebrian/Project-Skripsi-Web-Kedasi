ead46f10ebe04005498b1f94010cc40d5644f6f8
romadebrian <roma_coll04@yahoo.com>
2022-12-25 17:34:44 +0700

    change class component to functional component

--- src/pages/Pesan/ChatBox/ChatBox.js
+++ src/pages/Pesan/ChatBox/ChatBox.js
@@ -1,42 +1,44 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./ChatBox.css";
 import ChatFromAdmin from "./props/ChatFromAdmin";
 import ChatFromUser from "./props/ChatFromUser";
 
 import firebase from "../../../config/firebase";
+import { FormattingDateTime } from "../../../config/formattingDateTime";
 
-export default class ChatBox extends Component {
-  datanya = {
-    dataChat: "",
-  };
+export default function ChatBox(props) {
+  // datanya = {
+  //   dataChat: "",
+  // };
 
-  state = {
-    text: "",
-    IDYangLogin: JSON.parse(localStorage.getItem("UserId")),
-  };
+  // state = {
+  //   text: "",
+  //   userId: JSON.parse(localStorage.getItem("UserId")),
+  // };
 
-  handlechange = (params) => {
-    this.setState({ text: params.target.value });
-  };
+  const [userId] = useState(JSON.parse(localStorage.getItem("UserId")));
+  const [message, setMessage] = useState("");
 
-  handleEnter = (event) => {
+  // const handleEnter = (event) => {
+  //   event.preventDefault();
+  //   // console.log("Enter");
+  //   handleSendChat();
+  // };
+
+  const handleSendChat = (event) => {
     event.preventDefault();
-    // console.log("Enter");
-    this.handleSendChat();
-  };
 
-  handleSendChat = () => {
-    const IDchat = this.props.UID;
-    var tanggal = new Date().toUTCString();
+    const IDchat = props.UID;
+    var DateTimeNow = FormattingDateTime(new Date());
 
     firebase
       .database()
       .ref("chat/" + IDchat)
       .push(
         {
-          Nama: this.state.IDYangLogin,
-          Waktu: tanggal,
-          Pesan: this.state.text,
+          IDUser: userId,
+          Pesan: message,
+          Waktu: DateTimeNow,
         },
         (error) => {
           if (error) {
@@ -52,80 +54,78 @@ export default class ChatBox extends Component {
       );
   };
 
-  render() {
-    return (
-      <div className="card col-md-8 direct-chat direct-chat-primary">
-        <div className="card-header">
-          <h3 className="card-title">{this.props.dataUser[0].nama}</h3>
-        </div>
-        {/* /.card-header */}
-        <div className="card-body">
-          {/* Conversations are loaded here */}
-          <div className="direct-chat-messages">
-            {/* Chat */}
-
-            {this.props.chatData.length > 0 ? (
-              <Fragment>
-                {this.props.chatData.map((result) => {
-                  // console.log(result);
-
-                  console.log(result.data.Nama);
-                  console.log(this.state.IDYangLogin);
-                  if (result.data.Nama === this.state.IDYangLogin) {
-                    return (
-                      <ChatFromAdmin
-                        key={result.id}
-                        nama={this.props.dataAdmin[0].nama}
-                        waktu={result.data.Waktu}
-                        pesan={result.data.Pesan}
-                        photo={this.props.dataAdmin[0].photo}
-                      />
-                    );
-                  } else {
-                    return (
-                      <ChatFromUser
-                        key={result.id}
-                        nama={this.props.dataUser[0].nama}
-                        waktu={result.data.Waktu}
-                        pesan={result.data.Pesan}
-                        photo={this.props.dataUser[0].photo}
-                      />
-                    );
-                  }
-                })}
-              </Fragment>
-            ) : null}
-            {/* End of Chat */}
-          </div>
-          {/*/.direct-chat-messages*/}
-        </div>
-
-        {/* /.card-body */}
-        <div className="card-footer">
-          <form onSubmit={this.handleEnter}>
-            <div className="input-group">
-              <input
-                type="text"
-                name="message"
-                placeholder="Type Message ..."
-                className="form-control"
-                onChange={(e) => this.handlechange(e)}
-                // onKeyPress={this.handleEnter}
-              />
-              <span className="input-group-append">
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={(e) => this.handleSendChat(e)}
-                >
-                  Send
-                </button>
-              </span>
-            </div>
-          </form>
-        </div>
-        {/* /.card-footer*/}
+  return (
+    <div className="card col-md-8 direct-chat direct-chat-primary">
+      <div className="card-header">
+        <h3 className="card-title">{props.dataUser[0].nama}</h3>
       </div>
-    );
-  }
+      {/* /.card-header */}
+      <div className="card-body">
+        {/* Conversations are loaded here */}
+        <div className="direct-chat-messages">
+          {/* Chat */}
+
+          {props.chatData.length > 0 ? (
+            <Fragment>
+              {props.chatData.map((result) => {
+                console.log(props);
+
+                console.log(result);
+                console.log(userId);
+                if (result.data.IDUser === userId) {
+                  return (
+                    <ChatFromAdmin
+                      key={result.id}
+                      nama={props.dataAdmin[0].nama}
+                      waktu={result.data.Waktu}
+                      pesan={result.data.Pesan}
+                      photo={props.dataAdmin[0].photo}
+                    />
+                  );
+                } else {
+                  return (
+                    <ChatFromUser
+                      key={result.id}
+                      nama={props.dataUser[0].nama}
+                      waktu={result.data.Waktu}
+                      pesan={result.data.Pesan}
+                      photo={props.dataUser[0].photo}
+                    />
+                  );
+                }
+              })}
+            </Fragment>
+          ) : null}
+          {/* End of Chat */}
+        </div>
+        {/*/.direct-chat-messages*/}
+      </div>
+
+      {/* /.card-body */}
+      <div className="card-footer">
+        <form onSubmit={handleSendChat}>
+          <div className="input-group">
+            <input
+              type="text"
+              name="message"
+              placeholder="Type Message ..."
+              className="form-control"
+              onChange={({ target }) => setMessage(target.value)}
+              // onKeyPress={handleEnter}
+            />
+            <span className="input-group-append">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={(e) => handleSendChat(e)}
+              >
+                Send
+              </button>
+            </span>
+          </div>
+        </form>
+      </div>
+      {/* /.card-footer*/}
+    </div>
+  );
 }
